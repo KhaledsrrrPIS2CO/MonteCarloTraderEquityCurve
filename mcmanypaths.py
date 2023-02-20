c12b9f3c7420ff1f63f8061cc7883a771980a129
Khaled Ameen <Khaled.ameen11@gmail.com>
2023-02-20 22:03:52 +0100

    trimming the def simulate_equity_curve

--- mcmanypaths.py
+++ mcmanypaths.py
@@ -3,30 +3,30 @@ import matplotlib.pyplot as plt
 import random
 import time
 
-# Define the parameters
+# Defining parameters
 start_time = time.time()
 # initial equity amount of the options trader
-initial_equity = 3000
+initial_equity = 100000
 # the percentage loss when a trade is not successful
 loss_pct = 0.01
 # the percentage gain when a trade is successful
-win_pct = 0.0095
+win_pct = 0.01
 # the win rate of the trader's trades
-win_rate = 0.42
+win_rate = 0.5
 # number of trades to be simulated
-n_simulations = 400
+n_simulations = 100000
 # number of runs or nuber of paths
-number_of_runs = 500000
+number_of_runs = 1000
 
-# the sudden loss interval is in the function to generate real random interval
+# Sudden loss interval is in the function to generate real random interval
 sudden_error_upper = 0.01  # the sudden loss upper rate
 sudden_error_lower = 0.01  # the sudden loss  lower rate
-# the sudden convex interval is in the function to generate real random interval
+# Sudden convex interval is in the function to generate real random interval
 convex_payoff_upper = 0.01  # upper bound for the random convex payoff
 convex_payoff_lower = 0.01  # lower bound for the random convex payoff
 
 
-# Define the simulation function to calculate the equity curve of the options trader
+# Defining the simulation function
 def simulate_equity_curve(initial_equity_loc, loss_pct_loc, win_pct_loc, win_rate_loc, n_simulations_loc):
     equity_array = np.zeros((n_simulations_loc,))  # Initialize an array to store the equity value at each trade
     equity_array[0] = initial_equity_loc  # Set the initial equity value
@@ -42,17 +42,15 @@ def simulate_equity_curve(initial_equity_loc, loss_pct_loc, win_pct_loc, win_rat
         win = np.random.binomial(1, win_rate_loc, 1)
 
         if win:
-            # If the trade was successful, calculate the daily return
-            daily_return = equity_counter * win_pct_loc
-            # Add  the daily return and commissions to the equity counter
-            equity_counter += daily_return
-            equity_counter += commissions
+            # Calculate the daily return for a successful trade
+            daily_return = equity_counter * win_pct
         else:
-            # If the trade was not successful, calculate the daily loss
-            daily_return = -equity_counter * loss_pct_loc
-            # Add the daily loss and commissions to the equity counter
-            equity_counter += daily_return
-            equity_counter += commissions
+            # Calculate the daily return for an unsuccessful trade
+            daily_return = -equity_counter * loss_pct
+
+        # Add the daily return and commissions to the equity counter
+        equity_counter += daily_return
+        equity_counter += commissions
 
         # Store the equity counter value in the equity array
         equity_array[i] = equity_counter
@@ -61,8 +59,9 @@ def simulate_equity_curve(initial_equity_loc, loss_pct_loc, win_pct_loc, win_rat
         if i % sudden_convex_interval == 0:
             # Generate a random convex payoff rate
             convex_payoff = np.random.uniform(convex_payoff_lower, convex_payoff_upper)
-            # Calculate the convex payoff
+            # Calculate and add the convex payoff
             convex_payoff_amount = equity_counter * convex_payoff
+            # Add the convex payoff  to the equity counter
             equity_counter += convex_payoff_amount
 
         # Introduce the sudden loss of a random percentage between 5% and 10% with any desired frequency
@@ -76,11 +75,11 @@ def simulate_equity_curve(initial_equity_loc, loss_pct_loc, win_pct_loc, win_rat
 
         if equity_counter <= 0:
             break
-
-    # Return the final equity array
     return equity_array
 
-n_runs = number_of_runs   # number of runs for simulations
+
+#  Running the simulation and storing the results
+n_runs = number_of_runs
 simulation_results = []
 
 for i in range(n_runs):
@@ -88,10 +87,11 @@ for i in range(n_runs):
     simulation_results.append(result)
 
 
-# Plot all the simulations on one graph
+# Plotting the simulation results:
 plt.figure(figsize=(7, 7))
 for i in range(n_runs):
     plt.plot(simulation_results[i], color=plt.cm.cool(i / n_runs), label="")
+
 
 # Calculate the minimum and maximum equity values across all simulations
 min_equity = np.min([result[-1] for result in simulation_results])
@@ -100,6 +100,7 @@ avg_equity = np.average([result[-1] for result in simulation_results])
 print("Min equity:", round(min_equity, 2), "\nAvg equity: ", round(avg_equity, 2)
       , "\nMax equity: ", round(max_equity, 2), )
 print("\nNumber of paths:", n_runs)
+
 
 # Plot the minimum and maximum equity values
 plt.plot([0, n_simulations-1], [max_equity, max_equity], color='green', label="max")
@@ -112,7 +113,8 @@ plt.text(0, min_equity, f'${min_equity:.0f}', fontsize="13")
 plt.text(0, max_equity, f'${max_equity:.0f}', fontsize="13")
 plt.text(0, avg_equity, f'${avg_equity:.0f}', fontsize="13")
 
-# probability of being above or below average equity
+
+# The probability of being above or below average equity
 n_above_avg = np.count_nonzero([result[-1] > avg_equity for result in simulation_results])
 p_above_avg = round(n_above_avg / n_runs * 100, 2)
 p_below_avg = 100 - p_above_avg
@@ -122,15 +124,12 @@ print("Probability of below avg equity :", p_below_avg, "%")
 
 #  The probability of a trader doubling his initial equity after number of simulations
 n_doubled = 0
-
 for i in range(n_runs):
     result = simulate_equity_curve(initial_equity, loss_pct, win_pct, win_rate, n_simulations)
     simulation_results.append(result)
     final_equity = result[-1]
     if final_equity >= 2 * initial_equity:
         n_doubled += 1
-
-# Calculate the probability of doubling initial equity
 p_doubled = (n_doubled / n_runs) * 100
 print("Probability of doubling initial equity:", p_doubled, "%")
 
@@ -140,7 +139,8 @@ variation_from_avg = [result[-1] - avg_equity for result in simulation_results]
 # Calculate the standard deviation of the variation from the average equity
 std_dev = np.std(variation_from_avg)
 std_dev_round = round(std_dev, 2)
-print("Standard deviation of variation from average equity:", std_dev_round)
+print("σ: One standard deviation of variation from average equity:", std_dev_round)
+
 
 # plot
 plt.title("Monte Carlo Simulation")
@@ -148,11 +148,11 @@ plt.ylabel("$$$$$")
 plt.xlabel("Num of simulations")
 plt.show()
 
+
 # Time needed for the whole MC simulation
 elapsed_time = time.time() - start_time
-rounded_time = round(elapsed_time, 2)
-print("Time elapsed: ", rounded_time, "seconds")
-
+rounded_time = round(elapsed_time, 2)/60
+print("Time elapsed: ", rounded_time, "minutes")
 
 exit()
 

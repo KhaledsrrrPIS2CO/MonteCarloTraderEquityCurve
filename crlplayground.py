4da4962e61170070898140da3b7a5cc094b516d0
Khaled Ameen <Khaled.ameen11@gmail.com>
2023-03-18 18:19:12 +0100

    fun get_player_stats will take decoded_players_tags_list and use it to retrieve all player statistics.

--- crlplayground.py
+++ crlplayground.py
@@ -1,95 +1,65 @@
 import requests
 
-# Obtain an API key from the Clash Royale Developer Portal
+
 api_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc' \
-          '3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY3ZDdiYTA1LWY1MWMtNDk4Zi05NTgyLTAxNjI4' \
-          'YzI5ZjJhOSIsImlhdCI6MTY3NzYxMjc2NSwic3ViIjoiZGV2ZWxvcGVyLzc3YWQ4NGY3LTFjNzItNjMwOC0yY2Y3LTliOGRkN2E3OTYw' \
-          'ZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5n' \
-          'In0seyJjaWRycyI6WyIyLjIwNS4xMS4yNDUiXSwidHlwZSI6ImNsaWVudCJ9XX0.7DuDrCFsJj-6gGj15I0hW6CsT2XX7tN5GVbphfT' \
-          'YwldBk7aDqs9c-JZHDVcWSfivZt9TJ9AaPGSHegDDrj68nw'
+           '3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY3ZDdiYTA1LWY1MWMtNDk4Zi05NTgyLTAxNjI4' \
+           'YzI5ZjJhOSIsImlhdCI6MTY3NzYxMjc2NSwic3ViIjoiZGV2ZWxvcGVyLzc3YWQ4NGY3LTFjNzItNjMwOC0yY2Y3LTliOGRkN2E3OTYw' \
+           'ZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5n' \
+           'In0seyJjaWRycyI6WyIyLjIwNS4xMS4yNDUiXSwidHlwZSI6ImNsaWVudCJ9XX0.7DuDrCFsJj-6gGj15I0hW6CsT2XX7tN5GVbphfT' \
+           'YwldBk7aDqs9c-JZHDVcWSfivZt9TJ9AaPGSHegDDrj68nw'
 
 
-def get_clans_above_score(api_key: str, min_score: int) -> list:
-    """
-    Retrieve the tags of all Clash Royale clans with a `clanScore` above `min_score`.
-
-    Parameters:
-    api_key (str): Your Clash Royale API key.
-    min_score (int): The minimum `clanScore` value that the returned clans should have.
-
-    Returns:
-    list: A list of clan tags (strings) for the clans with `clanScore` above `min_score`.
-
-    Raises:
-    ValueError: If the API returns an error status code.
-    """
-
-    # Construct the API request URL and headers
-    url = f"https://api.clashroyale.com/v1/clans?minScore={min_score}"
+def get_player_stats(tag: str, api_key: str) -> dict:
+    url = f"https://api.clashroyale.com/v1/players/{tag}/"
     headers = {
-        "Accept": "application/json",
-        "authorization": f"Bearer {api_key}"
+        "Authorization": f"Bearer {api_key}",
+        "Accept": "application/json"
     }
-
-    # Send the API request and handle the response
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-        items = response.json()["items"]
-        clan_tags = [item["tag"] for item in items]
-        return clan_tags
-    elif response.status_code == 400:
-        raise ValueError("Invalid input parameter(s)")
-    elif response.status_code == 401:
-        raise ValueError("Unauthorized access, check API key")
-    elif response.status_code == 403:
-        raise ValueError("Access denied, insufficient privileges")
-    elif response.status_code == 404:
-        raise ValueError("Resource not found")
+        data = response.json()
+        player_tag = data['tag']
+        player_name = data['name']
+        wins = data['wins']
+        losses = data['losses']
+        battle_count = data['battleCount']
+        sum_wins_losses = wins + losses
+        net_win_rate = round(wins / sum_wins_losses * 100, 2)
+        net_loss_rate = round(100 - net_win_rate, 2)
+        win_rate_with_discrepancy = round((wins / battle_count) * 100, 2)
+        loss_rate_with_discrepancy = round(100 - win_rate_with_discrepancy, 2)
+        discrepancy_games = battle_count - sum_wins_losses
+        discrepancy_pct = round(((battle_count - sum_wins_losses) / battle_count) * 100, 2)
+
+        return {
+            'player_tag': player_tag,
+            'name': player_name,
+            'net_win_rate': net_win_rate,
+            'net_loss_rate': net_loss_rate,
+            'win_rate_with_discrepancy': win_rate_with_discrepancy,
+            'loss_rate_with_discrepancy': loss_rate_with_discrepancy,
+            'discrepancy_games': discrepancy_games,
+            'discrepancy_pct': discrepancy_pct
+        }
+
     else:
-        raise ValueError("Unknown error occurred")
+        raise ValueError(f"Error Code for Player {tag}: {response.status_code}")
+
+
+player_tags = ["%23QCR929GGQ", "%23G9YV9GR8R"]
+
+player_stats_list = []
+for tag in player_tags:
+    player_stats = get_player_stats(tag, api_key)
+    player_stats_list.append(player_stats)
+
+for player in player_stats_list:
+    print(f"Player tag: {player['player_tag']}")
+    print(f"Net win rate: {player['net_win_rate']:.2f}%")
+    print(f"Net loss rate: {player['net_loss_rate']:.2f}%")
+    print("\n_______________________")
 
 
 
-# Call get_clans_above_score
-min_score = 74000
-clan_tags_with_score_above = get_clans_above_score(api_key, min_score)
-print(f"Retrieved {len(clan_tags_with_score_above)} Clan tags with score above {min_score}.")
-print("____")
 
-def get_clan_details(api_key, clan_tag):
-    url = f"https://api.clashroyale.com/v1/clans/{clan_tag}"
-    headers = {"Authorization": f"Bearer {api_key}"}
-
-    response = requests.get(url, headers=headers)
-    data = response.json()
-
-    if "name" not in data:
-        print(f"Error: API response for clan tag {clan_tag} does not contain 'name' key")
-        return None
-
-    clan_details = {
-        "name": data["name"],
-        "tag": data["tag"],
-        "description": data.get("description", ""),
-        "type": data.get("type", ""),
-        "location": {
-            "name": data.get("location", {}).get("name", ""),
-            "code": data.get("location", {}).get("id", "")
-        },
-        "members": data.get("members", 0),
-        "score": data.get("score", 0),
-        "donations": data.get("donations", 0),
-        "clan_war_trophies": data.get("clanWarTrophies", 0),
-        "required_trophies": data.get("requiredTrophies", 0),
-        "clan_chest_level": data.get("clanChestLevel", 0),
-        "clan_chest_max_level": data.get("clanChestMaxLevel", 0)
-    }
-
-    return clan_details
-
-
-clan_tag = "%239YLRLG09"
-clan_details = get_clan_details(api_key, clan_tag)
-print("Clan details:", clan_details)
-
-exit()
--- crtoponemil.py
+++ crtoponemil.py
@@ -52,7 +52,7 @@ def get_clans_above_score(api_key: str, min_score: int) -> list:
 
 
 # Call get_clans_above_score
-min_score = 74000
+min_score = 75000
 clan_tags_with_score_above = get_clans_above_score(api_key, min_score)
 print(f"Retrieved {len(clan_tags_with_score_above)} Clan tags with score above {min_score}.")
 print("____")
@@ -111,8 +111,60 @@ def decoding_players_tags(player_tags):
 
 
 # Call decoding_players_tags
-decoded_players_tags = decoding_players_tags(player_tags)
-print(f"Decoded {len(decoded_players_tags)} players tags.")
+decoded_players_tags_list = decoding_players_tags(player_tags)
+print(f"Decoded {len(decoded_players_tags_list)} players tags.")
 print("_____")
+
+
+def get_player_stats(tag: str, api_key: str) -> dict:
+    url = f"https://api.clashroyale.com/v1/players/{tag}/"
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Accept": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        player_tag = data['tag']
+        player_name = data['name']
+        wins = data['wins']
+        losses = data['losses']
+        battle_count = data['battleCount']
+        sum_wins_losses = wins + losses
+        net_win_rate = round(wins / sum_wins_losses * 100, 2)
+        net_loss_rate = round(100 - net_win_rate, 2)
+        win_rate_with_discrepancy = round((wins / battle_count) * 100, 2)
+        loss_rate_with_discrepancy = round(100 - win_rate_with_discrepancy, 2)
+        discrepancy_games = battle_count - sum_wins_losses
+        discrepancy_pct = round(((battle_count - sum_wins_losses) / battle_count) * 100, 2)
+
+        return {
+            'player_tag': player_tag,
+            'name': player_name,
+            'net_win_rate': net_win_rate,
+            'net_loss_rate': net_loss_rate,
+            'win_rate_with_discrepancy': win_rate_with_discrepancy,
+            'loss_rate_with_discrepancy': loss_rate_with_discrepancy,
+            'discrepancy_games': discrepancy_games,
+            'discrepancy_pct': discrepancy_pct
+        }
+
+    else:
+        raise ValueError(f"Error Code for Player {tag}: {response.status_code}")
+
+
+player_tags = decoded_players_tags_list
+
+player_stats_list = []
+for tag in player_tags:
+    player_stats = get_player_stats(tag, api_key)
+    player_stats_list.append(player_stats)
+
+for player in player_stats_list:
+    print(f"Player tag: {player['player_tag']}")
+    print(f"Net win rate: {player['net_win_rate']:.2f}%")
+    print(f"Net loss rate: {player['net_loss_rate']:.2f}%")
+    print("\n_______________________")
 
 exit()
